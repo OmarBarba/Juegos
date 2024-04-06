ad4351bb97ca841524ffbff163e3a0a8101209cf
Omar Alejandro Barba Limón <barbalimonomar@gmail.com>
2024-04-06 13:28:56 -0600

    update

--- clue/especificos/scripfin.js
+++ clue/especificos/scripfin.js
@@ -1,26 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
     var contenido = [
-        { imagen: "imagen1.jpg", texto: "Texto de la imagen 1." },
-        { imagen: "imagen2.jpg", texto: "Texto de la imagen 2." },
-        { imagen: "imagen3.jpg", texto: "Texto de la imagen 3." }
+        { imagen: "/clue/especificos/imagenes/bosque.jpeg", texto: "Cuando de pronto apareces en la entrada a un bosque no sabes que es lo que esta pasando pero se escucha una musica dentro de la puerta donde entras para ver que esta pasando y te encuentras con 5 peculiares personajes. " },
+        { imagen: "/clue/especificos/imagenes/personajes2.jpeg", texto: "Los personajes que estan en frente tuya son: Una bruja el cual lleva con una cadena a un Ogro donde la Bruja dice 'No tengas miedo el sabe comportarse bueno la mayor parte del tiempo', Un cazador el cual lleva un escudo, un Mago y un Hobbit." },
+        { imagen: "/clue/especificos/imagenes/caminos2.jpeg", texto: "Del otro lado del que estaban los personajes estan 5 caminos los cuales llevana diferentes partes del bosque, los personajes toman su camino por diferentes partes del bosque.Cuando de pronto todo se pone en silecion y desaparece la luz y se escucha a alguien gritar.  " }
     ];
 
     var currentIndex = 0;
     var imagenElement = document.getElementById("imagen");
     var parrafoElement = document.getElementById("parrafo");
+    var videoElement = document.getElementById("video");
 
     function cambiarContenido() {
-        // Obtener el objeto de contenido actual
         var contenidoActual = contenido[currentIndex];
-
-        // Asignar la imagen y el texto al HTML
         imagenElement.src = contenidoActual.imagen;
         parrafoElement.textContent = contenidoActual.texto;
-
-        // Incrementar el índice o volver al inicio si es necesario
         currentIndex = (currentIndex + 1) % contenido.length;
     }
+    function mostrarVideo(){
+        imagenElement.style.display = "none"; // Ocultar imagen
+        parrafoElement.style.display = "none"; // Ocultar párrafo
+        videoElement.style.display = "block"; // Mostrar video
+    }
 
-    // Cambiar el contenido cada 5 segundos (5000 milisegundos)
-    setInterval(cambiarContenido, 5000);
+    setInterval(cambiarContenido, 1000);
 });
