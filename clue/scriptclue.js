65a90014fb9faeacdbfa729fa65fcaa6d5f2bee7
Omar Alejandro Barba Limón <barbalimonomar@gmail.com>
2024-04-06 09:52:58 -0600

    adaptar el cambio para la selecion

--- clue/scriptclue.js
+++ clue/scriptclue.js
@@ -3,16 +3,12 @@ document.addEventListener("DOMContentLoaded", function() {
         "Dentro del juego vas a encontrar 5 localidades, 5 personajes y 5 armas tu debes de detectar la forma por la que se encuentra al asecino.",
         "Despues de haber pasado por eso debes de pasar al area donde vas a decidir si vas a ir al mapa o a ver a los personajes y ver sus historias.",
         "Teniendo ahora las reglas pasemos a la la historia todas las historias empiezan de la misma manera:",
-        "Estas en tú casa donde de pronto te pones a leer un libro antiguo que alguien avento afuera de tu casa el cual llevaba por nombre el asecinato casi perfecto dentro del Bosque Encantado. ",
+        "Estas en tú casa donde de pronto te pones a leer un libro antiguo que alguien avento afuera de tu casa el cual llevaba por nombre el asecinato casi perfecto dentro del Bosque Encantado. "
         
 
     ];
 
-    var currentIndex = 0;
-    var parrafo = document.getElementById("parrafo");
-
-    setInterval(function() {
-        currentIndex = (currentIndex + 1) % contenido.length;
+    function cambiarParrafo() {
         // Añade clase para desvanecer el texto
         parrafo.classList.add("fade-out");
         setTimeout(function() {
@@ -20,7 +16,20 @@ document.addEventListener("DOMContentLoaded", function() {
             parrafo.textContent = contenido[currentIndex];
             // Elimina la clase de desvanecimiento después de un breve retraso
             parrafo.classList.remove("fade-out");
-        }, 1000); // Tiempo de espera para que se complete la transición
-    }, 20000); // Cambiar el contenido cada 5 segundos
+            // Incrementa el índice para el siguiente párrafo
+            currentIndex = (currentIndex + 1) % contenido.length;
+            // Detiene el cambio automático después de mostrar todos los párrafos una vez
+            if (currentIndex === 0) {
+                clearInterval(intervalID);
+            }
+            
+    var intervalID = setInterval(cambiarParrafo, 1000);
+
+    botonSeleccion.addEventListener("click", function() {
+        if (cambioCompletado) {
+            // Redirige a la página de selección solo si todos los párrafos ya se han mostrado una vez
+            window.location.href = "seleccion.html";
+        }
+    });
 });
 
