d3bfeb69fc5a752a21f058ec4db57574af39cdfc
Omar Alejandro Barba Limón <barbalimonomar@gmail.com>
2024-04-05 18:34:27 -0600

    Update para la explicacion del juego

--- clue/scriptclue.js
+++ clue/scriptclue.js
@@ -1,18 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
     var contenido = [
-        "Primer texto alternativo.",
-        "Segundo texto alternativo.",
-        "Tercer texto alternativo."
+        "Dentro del juego vas a encontrar 5 localidades, 5 personajes y 5 armas tu debes de detectar la forma por la que se encuentra al asecino.",
+        "Despues de haber pasado por eso debes de pasar al area donde vas a decidir si vas a ir al mapa o a ver a los personajes y ver sus historias.",
+        "Teniendo ahora las reglas pasemos a la la historia todas las historias empiezan de la misma manera:",
+        "Estas en tú casa donde de pronto te pones a leer un libro antiguo que alguien avento afuera de tu casa el cual llevaba por nombre el asecinato casi perfecto dentro del Bosque Encantado. ",
+        
+
     ];
 
     var currentIndex = 0;
+    var parrafo = document.getElementById("parrafo");
 
     setInterval(function() {
-        // Incrementa el índice del contenido y asegúrate de que no exceda la longitud del array
         currentIndex = (currentIndex + 1) % contenido.length;
-        // Obtén el párrafo por su id y actualiza su contenido con el texto correspondiente en el array
-        document.getElementById("parrafo").textContent = contenido[currentIndex];
-    }, 5000); // Cambiar el contenido cada 5 segundos (5000 milisegundos)
+        // Añade clase para desvanecer el texto
+        parrafo.classList.add("fade-out");
+        setTimeout(function() {
+            // Cambia el contenido del párrafo
+            parrafo.textContent = contenido[currentIndex];
+            // Elimina la clase de desvanecimiento después de un breve retraso
+            parrafo.classList.remove("fade-out");
+        }, 1000); // Tiempo de espera para que se complete la transición
+    }, 20000); // Cambiar el contenido cada 5 segundos
 });
-
 
